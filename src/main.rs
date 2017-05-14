432e82ad2dc1a051f3ab377aafcb8f6c49136cd0
Ruan L. Nunes <ruanrln@gmail.com>
2017-05-14 16:31:59 -0300

    save state

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 extern crate cgmath;
 extern crate bmp;
-use cgmath::{Point3,Vector3,InnerSpace,EuclideanSpace,MetricSpace};
-use bmp::{Image,Pixel};
+use cgmath::{Point3, Vector3, InnerSpace, EuclideanSpace, MetricSpace};
+use bmp::{Image, Pixel};
 use std::collections::LinkedList;
 use std::f32;
 use std::fmt::Debug;
 
+
 #[derive(Debug)]
 struct Ray {
     origin: Point3<f32>,
-    direction: Vector3<f32>
+    direction: Vector3<f32>,
 }
 
 impl Ray {
     fn point_at_parameter(&self, t: f32) -> Point3<f32> {
         self.origin + t * self.direction
+    }
+
+    fn trace(&self, scene: &Scene) -> Color {
+        let mut iter = scene.objects.iter();
+        let mut min_distance = f32::INFINITY;
+        let mut final_color = Color { r: 0, g: 0, b: 0 };
+        while let Some(t) = iter.next() {
+            if let Some(point) = t.hit(self) {
+                let distance = point.distance(self.origin);
+                if distance < min_distance {
+                    min_distance = distance;
+                    final_color = t.get_difuse_color();
+                }
+            }
+        }
+        return final_color;
     }
 }
 
@@ -29,29 +46,35 @@ struct Triangle {
     //n0: Vector3<f32>,
     //n1: Vector3<f32>,
     //n2: Vector3<f32>,
-    difuse_color: Color
+    difuse_color: Color,
 }
 
 #[derive(Debug)]
 struct Sphere {
     c: Point3<f32>,
     r: f32,
-    difuse_color: Color
+    difuse_color: Color,
+}
+
+#[derive(Debug)]
+struct Plane {
+    p: Point3<f32>,
+    n: Vector3<f32>,
 }
 
 #[derive(Debug)]
 struct Light {
     o: Point3<f32>,
-    color: Color
+    color: Color,
 }
 
 #[derive(Debug)]
 struct Scene {
     objects: LinkedList<Box<DrawableObject>>,
-    lights: LinkedList<Light>
+    lights: LinkedList<Light>,
 }
 
-trait DrawableObject : Debug {
+trait DrawableObject: Debug {
     // this functions return None when the Ray didn't hit the Object
     // and Some<Point3> when it hits, where Point3 is the hitpoint
     fn hit(&self, r: &Ray) -> Option<Point3<f32>>;
@@ -74,7 +97,7 @@ impl DrawableObject for Triangle {
         //verify if the triangle is in front of the ray
         let d = -(triangle_normal.dot(self.p0.to_vec()));
         let num = triangle_normal.dot(r.origin.to_vec()) + d;
-        let distance = -(num/den);
+        let distance = -(num / den);
         if distance < epsilon {
             return None;
         }
@@ -131,7 +154,7 @@ impl DrawableObject for Sphere {
             let dist = if d1 < d2 { d1 } else { d2 };
             let intersection_point = r.point_at_parameter(dist);
             Some(intersection_point)
-        } 
+        }
     }
 
     fn get_difuse_color(&self) -> Color {
@@ -143,43 +166,102 @@ impl DrawableObject for Sphere {
     }
 }
 
-// this function return the color of the point
-fn trace(r: &Ray, scene: &Scene) -> Pixel {
-    let mut iter = scene.objects.iter();
-    let mut min_distance = f32::INFINITY;
-    let mut final_color = Pixel {r: 0, g:0, b:0};
-    while let Some(t) = iter.next() {
-        if let Some(point) = t.hit(r) {
-            let distance = point.distance(r.origin);
-            if distance < min_distance {
-                min_distance = distance;
-                final_color = t.get_difuse_color();
-            }
-        }
-    }
-    return final_color;
-}
 
 fn main() {
-    let s = Sphere{c: Point3{x:512.0, y: 0.0, z: 0.0}, r: 50.0, difuse_color: Color{r:0, g:0, b:(0.7 * 256.0) as u8}};
-    let s1 = Sphere{c: Point3{x:512.0, y: 150.0, z: 0.0}, r: 50.0, difuse_color: Color{r:0, g:255, b: 0}};
-    let s2 = Sphere{c: Point3{x:512.0, y: -150.0, z: 0.0}, r: 50.0, difuse_color: Color{r:(0.8 * 256.0) as u8, g:0, b:(0.4 * 256.0) as u8}};
-    let s3 = Sphere{c: Point3{x:350.0, y: 0.0, z: 0.0}, r: 20.0, difuse_color: Color{r:(0.4 * 256.0) as u8, g:(0.6 * 256.0) as u8, b:0}};
-    let s4 = Sphere{c: Point3{x:1024.0, y: 0.0, z: 0.0}, r: 150.0, difuse_color: Color{r:(0.5 * 256.0) as u8, g:(0.5 * 256.0) as u8, b:0}};
+    let s = Sphere {
+        c: Point3 {
+            x: 512.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        r: 50.0,
+        difuse_color: Color {
+            r: 0,
+            g: 0,
+            b: (0.7 * 256.0) as u8,
+        },
+    };
+    let s1 = Sphere {
+        c: Point3 {
+            x: 512.0,
+            y: 150.0,
+            z: 0.0,
+        },
+        r: 50.0,
+        difuse_color: Color {
+            r: 0,
+            g: 255,
+            b: 0,
+        },
+    };
+    let s2 = Sphere {
+        c: Point3 {
+            x: 512.0,
+            y: -150.0,
+            z: 0.0,
+        },
+        r: 50.0,
+        difuse_color: Color {
+            r: (0.8 * 256.0) as u8,
+            g: 0,
+            b: (0.4 * 256.0) as u8,
+        },
+    };
+    let s3 = Sphere {
+        c: Point3 {
+            x: 350.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        r: 20.0,
+        difuse_color: Color {
+            r: (0.4 * 256.0) as u8,
+            g: (0.6 * 256.0) as u8,
+            b: 0,
+        },
+    };
+    let s4 = Sphere {
+        c: Point3 {
+            x: 1024.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        r: 150.0,
+        difuse_color: Color {
+            r: (0.5 * 256.0) as u8,
+            g: (0.5 * 256.0) as u8,
+            b: 0,
+        },
+    };
     let mut objects = LinkedList::new();
     objects.push_back(Box::new(s) as Box<DrawableObject>);
     objects.push_back(Box::new(s1));
     objects.push_back(Box::new(s2));
     objects.push_back(Box::new(s3));
     objects.push_back(Box::new(s4));
-    let scene = Scene{objects: objects, lights: LinkedList::new()};
+    let scene = Scene {
+        objects: objects,
+        lights: LinkedList::new(),
+    };
     let (w, h) = (1280, 720);
     let mut img = Image::new(w, h);
-    let o = Point3 {x: -512f32, y: 0f32, z: 0f32};
+    let o = Point3 {
+        x: -512f32,
+        y: 0f32,
+        z: 0f32,
+    };
     for (x, y) in img.coordinates() {
-        let d = Vector3 {x: 512.0, y: (x as i32 - (w as i32/2)) as f32, z: ((h as i32/2) - y as i32) as f32}.normalize();
-        let r = Ray {origin: o, direction: d};
-        let ret = trace(&r, &scene);
+        let d = Vector3 {
+                x: 512.0,
+                y: (x as i32 - (w as i32 / 2)) as f32,
+                z: ((h as i32 / 2) - y as i32) as f32,
+            }
+            .normalize();
+        let r = Ray {
+            origin: o,
+            direction: d,
+        };
+        let ret = r.trace(&scene);
         img.set_pixel(x, y, ret);
     }
 
@@ -189,31 +271,100 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use cgmath::{Point3,Vector3};
+    use cgmath::{Point3, Vector3};
     use super::{Color, DrawableObject};
     #[test]
     fn hit_should_hit() {
-        let o = Point3{x: 0.0, y: 0.0, z: 0.0};
-        let d = Vector3{x:1.0, y: 0.0, z: 0.0};
-        let p0 = Point3{x: 10.0, y: 3.0, z: 0.0};
-        let p1 = Point3{x: 10.0, y: -1.0, z: 3.0};
-        let p2 = Point3{x: 10.0, y: -1.0, z: -3.0};
-        let p_expected = Point3{x: 10.0, y: 0.0, z: 0.0};
-        let r = super::Ray{origin: o, direction: d};
-        let t = super::Triangle{p0: p0, p1: p1, p2: p2, difuse_color: Color{r:255, g:0, b:0}};
+        let o = Point3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        let d = Vector3 {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        let p0 = Point3 {
+            x: 10.0,
+            y: 3.0,
+            z: 0.0,
+        };
+        let p1 = Point3 {
+            x: 10.0,
+            y: -1.0,
+            z: 3.0,
+        };
+        let p2 = Point3 {
+            x: 10.0,
+            y: -1.0,
+            z: -3.0,
+        };
+        let p_expected = Point3 {
+            x: 10.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        let r = super::Ray {
+            origin: o,
+            direction: d,
+        };
+        let t = super::Triangle {
+            p0: p0,
+            p1: p1,
+            p2: p2,
+            difuse_color: Color {
+                r: 255,
+                g: 0,
+                b: 0,
+            },
+        };
         let result = t.hit(&r);
         assert_eq!(result, Some(p_expected));
     }
     #[test]
     fn should_not_hit() {
-        let o = Point3{x: 0.0, y: 0.0, z: 0.0};
-        let d = Vector3{x:1.0, y: 0.0, z: 0.0};
-        let p0 = Point3{x: 10.0, y: 3.0, z: 0.0};
-        let p1 = Point3{x: 13.0, y: -1.0, z: 0.0};
-        let p2 = Point3{x: 7.0, y: -1.0, z: 0.0};
-        let r = super::Ray{origin: o, direction: d};
-        let t = super::Triangle{p0: p0, p1: p1, p2: p2, difuse_color: Color{r:255, g:0, b:0}};
+        let o = Point3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        let d = Vector3 {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        let p0 = Point3 {
+            x: 10.0,
+            y: 3.0,
+            z: 0.0,
+        };
+        let p1 = Point3 {
+            x: 13.0,
+            y: -1.0,
+            z: 0.0,
+        };
+        let p2 = Point3 {
+            x: 7.0,
+            y: -1.0,
+            z: 0.0,
+        };
+        let r = super::Ray {
+            origin: o,
+            direction: d,
+        };
+        let t = super::Triangle {
+            p0: p0,
+            p1: p1,
+            p2: p2,
+            difuse_color: Color {
+                r: 255,
+                g: 0,
+                b: 0,
+            },
+        };
         let result = t.hit(&r);
         assert_eq!(result, None);
     }
 }
+
